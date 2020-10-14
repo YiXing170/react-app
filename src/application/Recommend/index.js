a4f9d0c1bc65d8471bb638258321115ac3b72a2e
yijiliang <857719943@qq.com>
2020-10-14 23:35:18 +0800

    add loading和懒加载

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -6,16 +6,26 @@ import Scroll from '../../baseUI/scroll/index';
 
 import { connect } from "react-redux";
 import * as actionTypes from './store/actionCreators';
+import { forceCheck } from 'react-lazyload';
+import Loading from '../../baseUI/loading/index';
 
 function Recommend (props) {
-  const { bannerList, recommendList } = props;
+  const { bannerList, recommendList, enterLoading } = props;
 
   const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
 
   // 获取数据
   useEffect(() => {
-    getBannerDataDispatch();
-    getRecommendListDataDispatch();
+    // 如果页面有数据，则不发请求
+    //immutable 数据结构中长度属性 size
+    if (!bannerList.size) {
+      getBannerDataDispatch();
+    }
+    if (!recommendList.size) {
+      getRecommendListDataDispatch();
+    }
+    // getBannerDataDispatch();
+    // getRecommendListDataDispatch();
     //eslint-disable-next-line
   }, []);
 
@@ -24,12 +34,13 @@ function Recommend (props) {
 
   return (
     <Content>
-      <Scroll>
+      <Scroll onScroll={forceCheck}>
         <div>
           <Slider bannerList={bannerListJS}></Slider>
           <RecommendList recommendList={recommendListJS}></RecommendList>
         </div>
       </Scroll>
+      {enterLoading ? <Loading></Loading> : null}
     </Content>
   );
 }
@@ -40,6 +51,7 @@ const mapStateToProps = (state) => ({
   // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
   bannerList: state.getIn(['recommend', 'bannerList']),
   recommendList: state.getIn(['recommend', 'recommendList']),
+  enterLoading: state.getIn(['recommend', 'enterLoading'])
 });
 // 映射 dispatch 到 props 上
 const mapDispatchToProps = (dispatch) => {
--- src/application/Recommend/store/actionCreators.js
+++ src/application/Recommend/store/actionCreators.js
@@ -11,6 +11,11 @@ export const changeRecommendList = (data) => ({
   type: actionTypes.CHANGE_RECOMMEND_LIST,
   data: fromJS(data)
 });
+export const changeEnterLoading = (data) => ({
+  type: actionTypes.CHANGE_ENTER_LOADING,
+  data: fromJS(data)
+});
+
 
 export const getBannerList = () => {
   return (dispatch) => {
@@ -26,6 +31,7 @@ export const getRecommendList = () => {
   return (dispatch) => {
     getRecommendListRequest().then(data => {
       dispatch(changeRecommendList(data.result));
+      dispatch(changeEnterLoading(false));// 改变 loading
     }).catch(() => {
       console.log("推荐歌单数据传输错误");
     });
